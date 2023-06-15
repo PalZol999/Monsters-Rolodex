ae7fa05ea80e8e2f90d6b148e1049bee53befa23
Administrator <p.z.palasti@gmail.com>
2023-06-15 11:06:54 +0200

    update 2.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react'
+import { useState, useEffect } from 'react';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box-component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+
+  const [searchField, setSearchField] = useState('') // [1st value, is the value that we want to store, the 2sd setValue function]
+  const [monsters, setMonsters] = useState([])
+  const [filterdMonsters, setFilterMonsters] = useState(monsters)
+
+  useEffect(()=> {  fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response)=> response.json())
+  .then ((users)=> setMonsters(users))}, [])
+
+  useEffect(()=> {  
+    const newFilterdMonsters= monsters.filter((monster)=> {
+    return monster.name.toLocaleLowerCase().includes(searchField)  
+  })
+  setFilterMonsters(newFilterdMonsters)
+  }, [monsters,searchField])
+
+  const onSearchChange =(event)=> {
+    const searchFieldString =event.target.value.toLocaleLowerCase() //58.video re! 
+    setSearchField(searchFieldString)
+    }
+
+   return  <div className="App">
+    <h1 className='app-title'>Monsters Roldex</h1>
+  
+
+   <SearchBox 
+   onChangeHandler={onSearchChange} 
+   placeholder='search monster' 
+   className='monsters-search-box'/>
+
+   
+   <CardList monsters={filterdMonsters}/>
+   
     </div>
-  );
-}
+  ;
+  } 
 
 export default App;
---
+++ src/components/search-box/search-box-component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './search-box-styles.css'   
+
+const SearchBox= ({className, placeholder,onChangeHandler}) => (
+ 
+    <input 
+    className={`search-box ${className}`} 
+    type='search'                                    
+    placeholder={placeholder} 
+    onChange={onChangeHandler}
+    ></input>                                   
+    
+ 
+)
+
+export default SearchBox
