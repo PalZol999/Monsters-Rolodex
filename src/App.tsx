e1d6932347479867c1f8614014530f8710511026
Administrator <p.z.palasti@gmail.com>
2023-07-15 14:04:53 +0200

    TS convert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
+
+import React from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box-component';
+import {getData} from './utils/data.utils'
 import './App.css';
 
+export type Monster = {
+  id: string
+  name: string
+  email: string
+}
 
 const App = () => {
-
   const [searchField, setSearchField] = useState("") // [1st value, is the value that we want to store, the 2sd setValue function]
-  const [monsters, setMonsters] = useState([])
+  const [monsters, setMonsters] = useState<Monster[]>([])
   const [filterdMonsters, setFilterMonsters] = useState(monsters)
 
-  useEffect(()=> {  fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response)=> response.json())
-  .then ((users)=> setMonsters(users))}, [])
+  useEffect(()=> {  
+  const fetchUser = async () => {
+    const users = await getData<Monster[]>('https://jsonplaceholder.typicode.com/users')
+    setMonsters(users)
+  }
+  fetchUser()
+},[])
 
   useEffect(()=> {  
     const newFilterdMonsters= monsters.filter((monster)=> {
@@ -23,7 +33,7 @@ const App = () => {
   setFilterMonsters(newFilterdMonsters)
   }, [monsters,searchField])
 
-  const onSearchChange =(event)=> {
+  const onSearchChange =(event: ChangeEvent<HTMLInputElement>): void=> {
     const searchFieldString =event.target.value.toLocaleLowerCase() //58.video re! 
     setSearchField(searchFieldString)
     }
--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './card-list-style.css'
+import {Monster} from '../../App'
 import Card from '../card/card.component'
 
+type CardListProps = {
+    monsters : Monster []
+  }
 
-const CardList= ({monsters}) => (
+const CardList= ({monsters}: CardListProps) => (
    
     <div className="card-list">
          {monsters.map((monster) => {    
